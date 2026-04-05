158da9948d8c4d0fae47d280abde49452ac8b697
amarkatoliya <amarbharavad994@gmail.com>
2026-04-05 17:44:59 +0530

    product module add

---
+++ app/Models/Core/Request.php
@@ -0,0 +1,66 @@
+<?php
+class models_Core_Request{
+    public function get($key,$default=null){
+        if(array_key_exists($key,$_GET)){
+            return $_GET[$key];
+        }
+        return $default;
+    }
+    public function post($key,$default=null){
+        if(array_key_exists($key,$_POST)){
+            return $_POST[$key];
+        }
+        return $default;
+    }
+
+    public function input($key,$default=null){
+        if(!$_GET[$key]){
+            return $_GET[$key];
+        }
+        if(!$_POST[$key]){
+            return $_POST[$key];
+        }
+        
+    }
+    public function has($key){
+        if($_GET[$key] || $_POST[$key]){
+            return true;
+        }
+        return false;
+    }
+    public function only($keys = []){
+        $data = $_REQUEST;
+        return array_intersect_key($data, array_flip($keys));
+    }
+    public function except($keys = []){
+        $data = $_REQUEST;
+        foreach($keys as $key){
+            unset($data[$key]);
+        }
+        return $data;
+    }
+    public function header($key,$default=null){
+        if(!$_SERVER['HTTP_'.$key]){
+            return $default;
+        }
+        return $_SERVER['HTTP_'.$key];
+    }
+    public function headers(){
+        return $_SERVER;
+    }
+    public function isPost(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function isGet(){
+        if($_SERVER['REQUEST_METHOD'] == 'GET'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+?>
--- app/boot.php
+++ app/boot.php
@@ -1,8 +1,14 @@
 <?php
 
-class Boot 
-{
-    public static function init() {
-        echo "222";
+require_once "app/controllers/Core/Base.php";
+require_once "app/models/Core/Request.php";
+require_once "app/controllers/Product.php";
+
+class Boot extends Controllers_Core_Base{
+    public static function init(){
+        $request = new models_Core_Request();
+        $controller = "Controllers_" . ucfirst($request->get("c","index"));
+        $controllerObject = new $controller(); 
+        $controllerObject->dispatch();
     }
 }
