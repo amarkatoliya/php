ac6bde6a8f170416eac369c29f4fd29473500c83
amarkatoliya <amarbharavad994@gmail.com>
2026-04-06 09:24:12 +0530

    minor change in controllers

---
+++ app/Models/Category.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "app/models/Core/table.php";
+
+class models_Category extends models_Core_Table{
+    public $tableName = 'category';
+    public $primaryKey = 'category_id';
+}
